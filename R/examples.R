1811dad63c112b2b86153a13d86bc66c37635d28
jedick <jedick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-02-07 06:43:08 +0000

    add metal-glycinate complexes from Azadi et al., 2019 and demo/glycinate.R
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@384 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/examples.R
+++ R/examples.R
@@ -27,8 +27,8 @@ examples <- function(save.png=FALSE) {
 }
 
 demos <- function(which=c("sources", "protein.equil", "affinity", "NaCl", "density", 
-  "ORP", "revisit", "findit", "ionize", "buffer", "protbuff", "yeastgfp", "mosaic",
-  "copper", "solubility", "gold", "wjd", "bugstab", "Shh", "saturation",
+  "ORP", "revisit", "findit", "ionize", "buffer", "protbuff", "yeastgfp", "glycinate",
+  "mosaic", "copper", "solubility", "gold", "wjd", "bugstab", "Shh", "saturation",
   "adenine", "DEW", "lambda", "TCA", "go-IU", "bison"), save.png=FALSE) {
   # run one or more demos from CHNOSZ with ask=FALSE, and return the value of the last one
   for(i in 1:length(which)) {
---
+++ demo/glycinate.R
@@ -0,0 +1,67 @@
+# CHNOSZ/demo/glycinate.R
+# plot logK of metal-glycinate complexes
+# 20190207
+
+# divalent metals
+di <- c("Cu+2", "Ni+2", "Co+2", "Mn+2", "Zn+2", "Cd+2")
+# divalent metals with one glycinate
+di1 <- c("Cu(Gly)+", "Ni(Gly)+", "Co(Gly)+", "Mn(Gly)+", "Zn(Gly)+", "Cd(Gly)+")
+# divalent metals with two glycinates
+di2 <- c("Cu(Gly)2", "Ni(Gly)2", "Co(Gly)2", "Mn(Gly)2", "Zn(Gly)2", "Cd(Gly)2")
+# monovalent metals
+mo <- c("Au+", "Ag+", "Na+", "Tl+", "Cu+")
+# monovalent metals with one glycinate
+mo1 <- c("Au(Gly)", "Ag(Gly)", "Na(Gly)", "Tl(Gly)", "Cu(Gly)")
+# monovalent metals with two glycinates
+mo2 <- c("Au(Gly)2-", "Ag(Gly)2-", "Na(Gly)2-", "Tl(Gly)2-", "Cu(Gly)2-")
+
+# set the temperature values
+T <- seq(0, 150, 10)
+# calculate the logKs using data from Azadi et al., 2019
+# doi:10.1016/j.fluid.2018.10.002
+logK_di1 <- logK_di2 <- logK_mo1 <- logK_mo2 <- list()
+for(i in 1:length(di1)) logK_di1[[i]] <- subcrt(c(di[i], "glycinate", di1[i]), c(-1, -1, 1), T = T)$out$logK
+for(i in 1:length(di2)) logK_di2[[i]] <- subcrt(c(di[i], "glycinate", di2[i]), c(-1, -2, 1), T = T)$out$logK
+for(i in 1:length(mo1)) logK_mo1[[i]] <- subcrt(c(mo[i], "glycinate", mo1[i]), c(-1, -1, 1), T = T)$out$logK
+for(i in 1:length(mo2)) logK_mo2[[i]] <- subcrt(c(mo[i], "glycinate", mo2[i]), c(-1, -2, 1), T = T)$out$logK
+
+# calculate the logKs for divalent metals using data from Shock and Koretsky, 1995
+# doi:10.1016/0016-7037(95)00058-8
+add.obigt("OldAA")
+logK_di1_SK95 <- logK_di2_SK95 <- list()
+for(i in 1:length(di1)) logK_di1_SK95[[i]] <- subcrt(c(di[i], "glycinate", di1[i]), c(-1, -1, 1), T = T)$out$logK
+for(i in 1:length(di2)) logK_di2_SK95[[i]] <- subcrt(c(di[i], "glycinate", di2[i]), c(-1, -2, 1), T = T)$out$logK
+data(thermo)
+
+# set up the plots
+layout(matrix(1:6, byrow = TRUE, nrow = 2), widths = c(2, 2, 1))
+par(mar = c(4, 3.2, 2.5, 0.5), mgp = c(2, 1, 0), las = 1, cex = 0.8)
+xlab <- axis.label("T")
+ylab <- axis.label("logK")
+
+# first row: divalent metals
+matplot(T, sapply(logK_di1, c), type = "l", lwd = 2, lty = 1, xlab = xlab, ylab = ylab)
+matplot(T, sapply(logK_di1_SK95, c), type = "l", lwd = 2, lty = 2, add = TRUE)
+legend(-5, 7.7, c("Azadi et al., 2019", "Shock and Koretsky, 1995"), lty = c(1, 2), bty = "n", cex = 1)
+mtext(expression(M^"+2" + Gly^"-" == M*(Gly)^"+"), line = 0.5)
+matplot(T, sapply(logK_di2, c), type = "l", lwd = 2, lty = 1, xlab = xlab, ylab = ylab)
+matplot(T, sapply(logK_di2_SK95, c), type = "l", lwd = 2, lty = 2, add = TRUE)
+legend(-5, 14, c("Azadi et al., 2019", "Shock and Koretsky, 1995"), lty = c(1, 2), bty = "n", cex = 1)
+mtext(expression(M^"+2" + 2*Gly^"-" == M*(Gly)[2]), line = 0.5)
+plot.new()
+par(xpd = NA)
+legend("right", as.expression(lapply(di, expr.species)), lty = 1, col = 1:6, bty = "n", cex = 1.2, lwd = 2)
+
+# add overall title
+text(0, 1, "metal-\nglycinate\ncomplexes", cex = 1.3, font = 2)
+par(xpd = FALSE)
+
+# second row: monovalent metals
+matplot(T, sapply(logK_mo1, c), type = "l", lwd = 2, lty = 1, xlab = xlab, ylab = ylab)
+mtext(expression(M^"+" + Gly^"-" == M*(Gly)), line = 0.5)
+matplot(T, sapply(logK_mo2, c), type = "l", lwd = 2, lty = 1, xlab = xlab, ylab = ylab)
+mtext(expression(M^"+" + 2*Gly^"-" == M*(Gly)[2]^"-"), line = 0.5)
+plot.new()
+par(xpd = NA)
+legend("right", as.expression(lapply(mo, expr.species)), lty = 1, col = 1:5, bty = "n", cex = 1.2, lwd = 2)
+par(xpd = FALSE)
