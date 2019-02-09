739ebbf3321725aea1b88bc83ebdccb397ccaaab
jedick <jedick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-02-09 05:15:07 +0000

    info(): add OldAA.csv to list of optional data files for searching
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@389 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/testthat/test-recalculate.R
+++ tests/testthat/test-recalculate.R
@@ -2,7 +2,7 @@ context("recalculate")
 
 # SK95: Shock and Korestky, 1995
 # doi:10.1016/0016-7037(95)00058-8
-test_that("recalculated values from SK95 are correctly enetered in OBIGT", {
+test_that("recalculated values from SK95 are correctly entered in OBIGT", {
   # test added 20190206
   # thermodynamic data entries for amino acid glycinate and alanate complexes
   # (no longer in default database)
@@ -11,31 +11,31 @@ test_that("recalculated values from SK95 are correctly enetered in OBIGT", {
   iaa <- info(c("alanate", "glycinate"))
   iGly <- grep("\\(Gly\\)\\+$", thermo$obigt$name)
   iGly2 <- grep("\\(Gly\\)2$", thermo$obigt$name)
-  iAlan <- grep("\\(Alan\\)\\+$", thermo$obigt$name)
-  iAlan2 <- grep("\\(Alan\\)2$", thermo$obigt$name)
+  iAla <- grep("\\(Ala\\)\\+$", thermo$obigt$name)
+  iAla2 <- grep("\\(Ala\\)2$", thermo$obigt$name)
   # get values used in OBIGT
   aa_GHS_OBIGT <- thermo$obigt[iaa, c("G", "H", "S")]
   Gly_GHS_OBIGT <- thermo$obigt[iGly, c("G", "H", "S")]
   Gly2_GHS_OBIGT <- thermo$obigt[iGly2, c("G", "H", "S")]
-  Alan_GHS_OBIGT <- thermo$obigt[iAlan, c("G", "H", "S")]
-  Alan2_GHS_OBIGT <- thermo$obigt[iAlan2, c("G", "H", "S")]
+  Ala_GHS_OBIGT <- thermo$obigt[iAla, c("G", "H", "S")]
+  Ala2_GHS_OBIGT <- thermo$obigt[iAla2, c("G", "H", "S")]
   # get values from SK95
   SK95 <- system.file("extdata/thermo/SK95.csv", package="CHNOSZ")
   add.obigt(SK95)
   aa_GHS_SK95 <- thermo$obigt[iaa, c("G", "H", "S")]
   Gly_GHS_SK95 <- thermo$obigt[iGly, c("G", "H", "S")]
   Gly2_GHS_SK95 <- thermo$obigt[iGly2, c("G", "H", "S")]
-  Alan_GHS_SK95 <- thermo$obigt[iAlan, c("G", "H", "S")]
-  Alan2_GHS_SK95 <- thermo$obigt[iAlan2, c("G", "H", "S")]
+  Ala_GHS_SK95 <- thermo$obigt[iAla, c("G", "H", "S")]
+  Ala2_GHS_SK95 <- thermo$obigt[iAla2, c("G", "H", "S")]
   # calculate differences for alanate and glycinate
   # nb. round values to avoid floating-point difficulties with unique() etc.
-  Alan_GHS_Delta <- round(aa_GHS_SK95[1, ] - aa_GHS_OBIGT[1, ], 2)
+  Ala_GHS_Delta <- round(aa_GHS_SK95[1, ] - aa_GHS_OBIGT[1, ], 2)
   Gly_GHS_Delta <- round(aa_GHS_SK95[2, ] - aa_GHS_OBIGT[2, ], 2)
   # test that the differences are the same in the corresponding complexes
   expect_equivalent(Gly_GHS_Delta, unique(round(Gly_GHS_SK95 - Gly_GHS_OBIGT, 2)))
   expect_equivalent(Gly_GHS_Delta, unique(round((Gly2_GHS_SK95 - Gly2_GHS_OBIGT)/2, 2)))
-  expect_equivalent(Alan_GHS_Delta, unique(round(Alan_GHS_SK95 - Alan_GHS_OBIGT, 2)))
-  expect_equivalent(Alan_GHS_Delta, unique(round((Alan2_GHS_SK95 - Alan2_GHS_OBIGT)/2, 2)))
+  expect_equivalent(Ala_GHS_Delta, unique(round(Ala_GHS_SK95 - Ala_GHS_OBIGT, 2)))
+  expect_equivalent(Ala_GHS_Delta, unique(round((Ala2_GHS_SK95 - Ala2_GHS_OBIGT)/2, 2)))
   # clean up
   data(thermo)
 })
