232131886e43dd7ae1119099f2cd4c448f5bbe79
jedick <jedick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-05-30 14:31:10 +0000

    export new functions lNaCl(), lS(), lT(), lP(), lTP(), and lex()
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@476 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ R/util.legend.R
@@ -0,0 +1,15 @@
+# CHNOSZ/util.legend.R
+# functions for making legend text
+# 20190530 jmd first version
+
+lNaCl <- function(x, digits = 2) substitute(italic(m)[NaCl] == x~mol~kg^-1, list(x = round(x, digits)))
+
+lS <- function(x, digits = 3) substitute(sum(S) == x~mol~kg^-1, list(x = round(x, digits)))
+
+lT <- function(x, digits = 0) substitute(x~degree*C, list(x = round(x, digits)))
+
+lP <- function(x, digits = 0) if(identical(x, "Psat")) lP <- quote(italic(P)[sat]) else lP <- substitute(x~bar, list(x = round(x, digits)))
+
+lTP <- function(x, y, digits = 0) substitute(list(x, y), list(x = lT(x, digits), y = lP(y, digits)))
+
+lex <- function(...) as.expression(c(...))
