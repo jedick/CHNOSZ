cf9ee473a5b3a3fbce7f27c07bfb0e2ea0816466
jedick <jedick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2022-02-07 04:38:29 +0000

    Remove functions in util.blast.R and example files in extdata/bison
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@699 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/examples.R
+++ R/examples.R
@@ -6,7 +6,7 @@ examples <- function(save.png=FALSE) {
   # run all the examples in CHNOSZ documentation
   .ptime <- proc.time()
   topics <- c("thermo", "examples",
-    "util.array", "util.blast", "util.data", "util.expression", "util.legend", "util.plot",
+    "util.array", "util.data", "util.expression", "util.legend", "util.plot",
     "util.fasta", "util.formula", "util.misc", "util.seq", "util.units",
     "util.water", "taxonomy", "info", "retrieve", "add.OBIGT", "protein.info",
     "hkf", "water", "IAPWS95", "subcrt", "Berman",
