628709310748046a2690f33abb5c6481a607397f
jedick <jedick@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2022-02-20 09:03:54 +0000

    Submit version 1.4.3 to CRAN
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@707 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- inst/tinytest/test-AD.R
+++ inst/tinytest/test-AD.R
@@ -87,11 +87,11 @@ expect_equal(sort(info(iaq[abs(GAD - GOBIGT) > 900])$name), sort(c("HCl", "ethan
 if(!at_home()) exit_file("Skipping tests on CRAN")
 # This one fails on Windows (tolerance = 9 works) 20220208
 expect_equal(sout1$Cp[1:3], Cp_ref1[1:3], tolerance = 3, scale = 1, info = "AD produces correct values for CO2 along saturation curve")
-# This one fails on ATLAS and M1Mac on CRAN 20220210
+# This one fails on ATLAS and M1mac on CRAN 20220210
 expect_equal(sout1$Cp[4], Cp_ref1[4], tolerance = 800, scale = 1, info = "AD produces correct values for CO2 along saturation curve")
 # This one fails on ATLAS
 expect_equal(sout2$Cp, Cp_ref2, tolerance = 14, scale = 1, info = "AD produces correct values for CO2 at 1000 bar")
-# This one fails on M1Mac
+# This one fails on M1mac
 # g / cm3 / K^2
 expect_equal(CHNOSZ:::.d2rho1_dT2(298.15, 1, FALSE), -0.000009503, tolerance = 0.000007, scale = 1,
              info = "Fugacity, density, and density derivatives of H2O are close to values in Akinfiev and Diamond (2003)")
