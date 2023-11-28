253c7e373e01faaa4b1429c626a7a4f73fc84737
Jeffrey Dick <j3ffdick@gmail.com>
2023-11-28 12:25:23 +0000

    Add FAQ: How to I plot mineral buffers for pH?
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@814 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- vignettes/mklinks.sh
+++ vignettes/mklinks.sh
@@ -147,3 +147,4 @@ sed -i 's/<code>info()<\/code>/<code><a href="..\/html\/info.html" style="color:
 sed -i 's/<code>thermo.refs()<\/code>/<code><a href="..\/html\/util.data.html" style="color:\ green;">thermo.refs()<\/a><\/code>/g' FAQ.html
 sed -i 's/<code>subcrt()<\/code>/<code><a href="..\/html\/subcrt.html" style="color:\ green;">subcrt()<\/a><\/code>/g' FAQ.html
 sed -i 's/<code>check.GHS()<\/code>/<code><a href="..\/html\/util.data.html" style="color:\ green;">check.GHS()<\/a><\/code>/g' FAQ.html
+sed -i 's/<code>affinity()<\/code>/<code><a href="..\/html\/affinity.html" style="color:\ green;">affinity()<\/a><\/code>/g' FAQ.html
