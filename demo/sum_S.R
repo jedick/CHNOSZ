f84799442eb09ac17d2b286a166b96838babe913
Jeffrey Dick <j3ffdick@gmail.com>
2024-11-30 04:33:27 +0000

    Add solubility contours for Fe and Au to demo/sum_S.R
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@851 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- demo/sum_S.R
+++ demo/sum_S.R
@@ -1,56 +1,78 @@
 # CHNOSZ/demo/sum_S.R
-# 20240604 Fe-S-C-O-H log a (sum S) - pH      modified from mosaic.R
-# 20241127 Fe-S-O-H   logfO2 - log a (sum S)  added IS
+# 20240604 Fe-S-O-C     log a (sum S) - pH      modified from mosaic.R
+# 20241130 Fe-S-O + Au  logfO2 - log a (sum S)  added NaCl and solubility contours
 
 library(CHNOSZ)
 
-for(IS in c(0, 0.5)) {
+# Define conditions
+res <- 500
+loga_S <- c(-6, 0, res)
+logf_O2 <- c(-45, -20, res)
+pH <- 5
+T <- 300
+#P <- "Psat"
+P <- 1000
+#IS <- 0.8
 
-  # Define conditions
-  res <- 500
-  loga_S <- c(-6, 0, res)
-  logf_O2 <- c(-45, -20, res)
-  pH <- 5
-  T <- 300
-  P <- "Psat"
-  #IS <- 0
-  # Setup chemical system
-  # Use "oxygen" instead of "O2" to get the gas
-  basis(c("FeO", "SO4-2", "H2O", "H+", "oxygen"))
-  basis("pH", pH)
-  # Add aqueous species followed by minerals
-  species(c("Fe+2", "Fe+3", "HFeO2-"))
-  species(c("pyrrhotite", "pyrite", "hematite", "magnetite"), add = TRUE)
+# Calculate solution composition for 0.8 mol/kg NaCl
+# Based on activity of Cl- from Fig. 18 of Skirrow and Walsh (2002)
+m_NaCl = 0.8
+NaCl <- NaCl(m_tot = m_NaCl, T = T, P = P, pH = pH)
+IS <- NaCl$IS
 
-  # List basis species to swap through
-  bases <- list( c("SO4-2", "HSO4-", "HS-", "H2S") )
+# Setup chemical system
+# Use oxygen instead of O2 to get the gas
+basis(c("Fe", "SO4-2", "oxygen", "H+", "Cl-", "H2O"))
+basis("pH", pH)
+basis("Cl-", log10(NaCl$m_Cl))
+# Add minerals as formed species
+species(c("pyrrhotite", "pyrite", "hematite", "magnetite"))
 
-  # Start diagram for loga_Fe = -6
-  species(1:3, -6)
-  m1 <- mosaic(bases, "SO4-2" = loga_S, O2 = logf_O2, T = T, P = P, IS = IS)
-  diagram(m1$A.species, bold = TRUE)
+# List basis species to swap through
+bases <- list( c("SO4-2", "HSO4-", "HS-", "H2S") )
+# Calculate mosaic for Fe minerals
+m <- mosaic(bases, "SO4-2" = loga_S, O2 = logf_O2, T = T, P = P, IS = IS)
 
-  # Overlay lines and labels for loga_Fe = -5
-  species(1:3, -5)
-  m2 <- mosaic(bases, "SO4-2" = loga_S, O2 = logf_O2, T = T, P = P, IS = IS)
-  d <- diagram(m2$A.species, add = TRUE, names = NA, fill = NA, lty = 3)
+# Loop over metals for solubility contours
+for(metal in c("Fe", "Au")) {
 
-  # Add dashed line for water stability limti
+  # Plot diagram for Fe minerals and show sulfur species
+  d <- diagram(m$A.species, bold = TRUE, lwd = 2)
+  diagram(m$A.bases[[1]], add = TRUE, col = 4, col.names = 4, lty = 4, dx = -2.5, dy = c(-4, 0, 0, 6))
+  # Add water stability limit
   water.lines(d)
 
+  if(metal == "Au") {
+    # Put Au as the first basis species for solubility() calculation
+    basis(c("Au", "SO4-2", "oxygen", "H+", "Cl-", "H2O"))
+    basis("pH", pH)
+    # This is the species whose solubility we want to calculate
+    species("Au")
+    # These are the aqueous species that form by dissolving Au
+    iaq <- retrieve("Au", state = "aq", ligands = c("O", "H", "S", "Cl"))
+    # Make orange contours for Au
+    cols <- c(6, 7)
+  } else {
+    # For Fe, the basis species and formed species are already set up properly for solubility()
+    # These are the aqueous species that form by dissolving Fe minerals
+    iaq <- retrieve("Fe", state = "aq", ligands = c("O", "H", "S", "Cl"))
+    # Make red contours for Fe
+    cols <- c(2, 2)
+  }
+  # Calculate solubility and convert log molality to ppb
+  s <- solubility(iaq, bases = bases, "SO4-2" = loga_S, O2 = logf_O2, T = T, P = P, IS = IS, in.terms.of = metal)
+  sp <- convert(s, "ppb")
+  # Plot twice to get deeper colors
+  for(col in cols) {
+    diagram(sp, type = "loga.balance", contour.method = "flattest", levels = 10^(-3:5), add = TRUE, col = col, lty = 3, lwd = 1.8, cex = 1.2)
+  }
+
   # Add legends and title
-  TP <- describe.property(c("T", "P"), c(T, P))
-  PI <- c(bquote(pH == .(pH)), bquote(italic(I) == .(IS)~mol~kg^-1))
-  legend1 <- lex(TP, PI)
-  legend("topright", legend1, bty = "n")
-
-  Fe <- c(
-    bquote(10^-5~italic(m)~Fe^"+2"),
-    bquote(10^-6~italic(m)~Fe^"+2")
-  )
-  legend2 <- lex(Fe)
-  legend("topleft", legend2, lty = c(3, 1), bty = "n")
-
-  title(main = "Fe-S-O-H with summed S and ionic strength", font.main = 1)
+  T_P <- describe.property(c("T", "P"), c(T, P))
+  P_Cl <- c(bquote(pH == .(pH)), bquote(NaCl == .(m_NaCl)~mol~kg^-1))
+  legend <- lex(T_P, P_Cl)
+  legend("topright", legend, bty = "n")
+  main <- paste("Fe minerals with summed aq species: S (x-axis) and ppb", metal, "(contours)")
+  title(main, font.main = 1)
 
 }
