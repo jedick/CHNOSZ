35d9718365f277158ae3dd9310e066764c74c967
Jeffrey Dick <j3ffdick@gmail.com>
2024-12-24 13:16:34 +0000

    Move Knight et al. (2024) to optional data
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/chnosz/pkg/CHNOSZ@856 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/util.data.R
+++ R/util.data.R
@@ -272,6 +272,7 @@ check.OBIGT <- function() {
     else if(what == "AD") tdata <- read.csv(system.file("extdata/OBIGT/AD.csv", package = "CHNOSZ"), as.is = TRUE)
     else if(what == "GEMSFIT") tdata <- read.csv(system.file("extdata/OBIGT/GEMSFIT.csv", package = "CHNOSZ"), as.is = TRUE)
     else if(what == "IGEM24") tdata <- read.csv(system.file("extdata/OBIGT/IGEM24.csv", package = "CHNOSZ"), as.is = TRUE)
+    else if(what == "KBJ24") tdata <- read.csv(system.file("extdata/OBIGT/KBJ24.csv", package = "CHNOSZ"), as.is = TRUE)
     ntot <- nrow(tdata)
     # Where to keep the results
     DCp <- DV <- DG <- rep(NA, ntot)
@@ -310,6 +311,7 @@ check.OBIGT <- function() {
   out <- rbind(out, checkfun("AS04"))
   out <- rbind(out, checkfun("GEMSFIT"))
   out <- rbind(out, checkfun("IGEM24"))
+  out <- rbind(out, checkfun("KBJ24"))
   # Set differences within a tolerance to NA
   out$DCp[abs(out$DCp) < 1] <- NA
   out$DV[abs(out$DV) < 1] <- NA
@@ -394,8 +396,10 @@ dumpdata <- function(file = NULL) {
   GEMSFIT <- cbind(source = "GEMSFIT", dat)
   dat <- read.csv(system.file("extdata/OBIGT/IGEM24.csv", package = "CHNOSZ"), as.is = TRUE)
   IGEM24 <- cbind(source = "IGEM24", dat)
+  dat <- read.csv(system.file("extdata/OBIGT/KBJ24.csv", package = "CHNOSZ"), as.is = TRUE)
+  KBJ24 <- cbind(source = "KBJ24", dat)
   # Put it all together
-  out <- rbind(OBIGT, SUPCRT92, SLOP98, AS04, AD, DEW, GEMSFIT, IGEM24)
+  out <- rbind(OBIGT, SUPCRT92, SLOP98, AS04, AD, DEW, GEMSFIT, IGEM24, KBJ24)
   # Quote columns 2 (name) and 3 (abbrv) because they have commas for some entries
   if(!is.null(file)) write.csv(out, file, row.names = FALSE, quote = c(2, 3))
   else(return(out))
